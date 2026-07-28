152a151eb477714faecc11a591a45435d5e91c0c
Umaimazainab02 <umaimazainab429@gmail.com>
2026-07-28 21:57:23 +0500

    Added loop practice questions

--- app.js
+++ app.js
@@ -73,28 +73,81 @@ let email = document.getElementById("email");
 let male = document.getElementById("male");
 let female = document.getElementById("female");
 let select = document.getElementById("select");
-let upload = document.getElementById("address");
+let address = document.getElementById("address");
 let detail = document.getElementById("detail");
 let submit = document.getElementById("submit");
+let check = document.getElementById("check");
+let file = document.getElementById("file");
+
 
 submit.addEventListener("click", function () {
     if (name.value.trim() === "") {
         document.getElementById("nameerror").innerHTML = "Enter your name";
-        return;
     } else {
         document.getElementById("nameerror").innerHTML = "";
     }
     if (password.value.trim() === "") {
         document.getElementById("passerror").innerHTML = "Enter your password";
-        return;
 
     } else if (password.value.length < 8) {
         document.getElementById("passerror").innerHTML = "Enter at least 8 character";
-        return;
     }
 
     else {
         document.getElementById("passerror").innerHTML = "";
     }
 
+
+    if (!email.value.includes("@")) {
+        document.getElementById("emailerror").innerHTML = "Email must contain @";
+    }
+    else {
+        document.getElementById("emailerror").innerHTML = "";
+    }
+    if (select.value === "") {
+        document.getElementById("selecterror").innerHTML = "please select a city";
+    }
+    else {
+        document.getElementById("selecterror").innerHTML = "";
+    }
+
+    if (detail.value.trim() === "") {
+        document.getElementById("detailerror").innerHTML = "Write a paragraph";
+    }
+    else {
+        document.getElementById("detailerror").innerHTML = ""
+    }
+    if (address.value.trim() === "") {
+        document.getElementById("addresserror").innerHTML = "Enter an address";
+    }
+    else {
+        document.getElementById("addresserror").innerHTML = ""
+    }
+
+    if (!check.checked) {
+        document.getElementById("checkerror").innerHTML = "Please accept the terms and conditions";
+    } else {
+        document.getElementById("checkerror").innerHTML = ""
+    }
+
+    if (file.files.length === 0) {
+        document.getElementById("fileerror").innerHTML = "Please upload an image";
+    }
+    else {
+        document.getElementById("fileerror").innerHTML = "";
+    }
+
+
+    if (
+    name.value.trim() !== "" &&
+    password.value.length >= 8 &&
+    email.value.includes("@") &&
+    select.value !== "" &&
+    detail.value.trim() !== "" &&
+    address.value.trim() !== "" &&
+    check.checked &&
+    file.files.length !== 0
+) {
+    document.getElementById("submiterror").innerHTML="Submit Successfully!";
+}
 })
